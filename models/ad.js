f678f0134907a5707a34b39ff74fc534c9d6c3d9
miranda-zapata <miranda.zapata@gmail.com>
2023-01-06 17:11:46 +0100

    Bring Mongoose's conection from script

--- models/ad.js
+++ models/ad.js
@@ -1,4 +1,5 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
+import { mongooseConnect } from "../lib/connectMongoose.js";
 
 // Definición del esquema de los anuncios
 const adSchema = mongoose.Schema({
@@ -10,3 +11,6 @@ const adSchema = mongoose.Schema({
 });
 
 export const Ad = mongoose.model("Ad", adSchema);
+
+// Aseguramos la conexión a Mongo (a través de Mongoose) en cualquier momento en el que se use el modelo de datos
+await mongooseConnect();
