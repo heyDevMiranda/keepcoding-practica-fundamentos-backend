2968ec50b9a566163b680a08907be1715edf8340
miranda-zapata <miranda.zapata@gmail.com>
2023-01-06 17:11:16 +0100

    Add a try...catch

--- routes/ads.js
+++ routes/ads.js
@@ -1,14 +1,18 @@
-import { express } from "express";
-import { Ad } from "../models/ad";
+import express from "express";
+import { Ad } from "../models/ad.js";
 
-export const router = express.Router();
+export const adsRouter = express.Router();
 
 /* GET home page */
-router.get("/", async function (req, res, next) {
-  // Coge todos los ads
-  const result = await Ad.find();
-  // Le pone un header a la respuesta
-  res.setHeader("Content-type", "application/json");
-  // Envía la respuesta
-  res.end(JSON.stringify(result));
+adsRouter.get("/", async function (req, res, next) {
+  try {
+    // Coge todos los ads
+    const result = await Ad.find();
+    // Le pone un header a la respuesta
+    res.setHeader("Content-type", "application/json");
+    // Envía la respuesta
+    res.end(JSON.stringify(result));
+  } catch (error) {
+    console.log(error);
+  }
 });
